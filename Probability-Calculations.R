bdd2e8bb9ea2a9a89f0b614c24807547f8fe8831
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-05-10 21:25:48 +0200

    Initial commit

---
+++ Probability-Calculations.R
@@ -0,0 +1,17 @@
+# Exercise 1
+# For X ~ Binomial(size = 4, prob = 0.2)
+# Calculate P(X <= 1) using dbinom(x, size, prob, log = FALSE)
+dbinom(x=0, size=4, prob=0.2) + dbinom(x=1, size=4, prob=0.2);
+
+
+
+
+
+
+
+
+
+
+
+
+
