61a687897d6e488f941ff2bdfa3c62249244e480
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-05-10 21:26:24 +0200

    Add the solution for Exercise 2

--- Probability-Calculations.R
+++ Probability-Calculations.R
@@ -3,6 +3,11 @@
 # Calculate P(X <= 1) using dbinom(x, size, prob, log = FALSE)
 dbinom(x=0, size=4, prob=0.2) + dbinom(x=1, size=4, prob=0.2);
 
+# Exercise 2
+# For X ~ Binomial(size = 4, prob = 0.2)
+# Calculate P(X <= 1) using pbinom(q, size, prob, lower.tail = TRUE, log.p = FALSE)
+pbinom(q=1, size=4, prob=0.2, lower.tail=TRUE);
+
 
 
 
